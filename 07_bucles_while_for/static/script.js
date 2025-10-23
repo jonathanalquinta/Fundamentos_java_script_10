0e24858653703291d49e59c7348b21d8d84291c3
jonathanalquinta <jonathanalquinta@liceovvh.cl>
2025-10-23 15:11:10 -0300

    trabajo

---
+++ 07_bucles_while_for/static/script.js
@@ -0,0 +1,91 @@
+console.log("Conexion Exitosa...")
+
+/*contar numeros del 10 al 1
+*/
+
+//creacion funcion bucle while
+function ejecutarWhile() {
+    let resultado = "El resultado es:";
+    let count = 10;
+    while (count > 0) {
+        resultado += ` ${count}`;
+        console.log(count);
+        count--
+    }
+    alert(resultado);
+}
+
+function ejecutarFor() {
+    let resultado = "el resultado es:";
+    //inicio bucle
+    for (let i = 10; i > 0; i--) {
+        resultado += ` ${i}`;
+    }
+    alert(resultado);
+}
+
+
+
+//ejercicios 
+
+/*1️⃣ Bucle while: Imprimir del 1 a n (n es ingresado por teclado).   alert(resultado);    let resultado = "El resultado es:";
+Escribe un programa que utilice un bucle while para imprimir los números del 1 al n con un solo Alert.*/
+
+function contarHastaN() {
+    let resultado = "contamos:"
+    let n = parseInt(prompt("ingresar numero"));
+    let contador = 1;
+    while (contador <= n) {
+        console.log(contador)
+        alert(contador)
+        contador++;
+    }
+}
+
+/*2️⃣ Bucle while: Cuenta regresiva del n al 1 (n es ingresado por teclado)
+Crea un programa que muestre una cuenta regresiva desde n hasta 1 utilizando un bucle while.*/
+
+function cuentaRegresiva() {
+    let contador = parseInt(prompt("Ingresar Numero"));
+    let resultado = "contamos: "
+    while (contador > 0 ) {
+        resultado += ` ${contador}`;
+        contador--;
+    }
+    alert(resultado)
+}
+
+
+/*3️⃣ Bucle while: Sumar los n primeros números
+Usando un bucle for, calcula la suma de los números n primeros números  muestra el resultado en la consola.*/
+
+
+function sumarNumeros() {
+    let suma = 0;
+    let numero = parseInt(prompt("Ingresar numero"));
+    let contador = 1;
+    let resultado = "Suma: 0";
+    while (contador <= numero) {
+        suma += contador;
+        resultado += ` + ${contador}`;
+        contador++
+    }
+    alert (` ${resultado} = ${suma}`);
+}
+
+/*4️⃣ Bucle while: Imprimir múltiplos de 2 del 1 al n (n es el límite de números a encontrar)
+Escribe un programa que use un bucle for para imprimir los múltiplos de 2 que hay entre 1 y 10 (inclusive).*/
+
+function imprimirMultiplos(){
+    let numero = parseInt(prompt("Ingresar Numero"));
+    let count = 1;
+    let resultado = "El resultado es: "
+    while (count <= numero) {
+        count++
+
+        if (count %2 == 0) {
+            resultado += `${count}`
+        }
+    }
+    alert(`${resultado}`)
+}
